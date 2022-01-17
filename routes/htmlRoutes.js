43ea7b77d4361ca6d0f40372b9ed8b312771d1db
Buff4eyes <claudemokbel@gmail.com>
2022-01-17 14:13:46 -0800

    htmlRoutes completed

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const path = require('path');
 
+//get home routes
+router.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 //  GET /notes - Should return the notes.html file.
-
+router.get('/notes', (req,res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
 
 module.exports = router;
