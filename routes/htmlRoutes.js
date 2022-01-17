3b5f89adc93d98973e9c72ffc02f8bb51b666de1
Buff4eyes <claudemokbel@gmail.com>
2022-01-17 13:16:56 -0800

    server.js

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const path = require('path');
+
+
+//  GET /notes - Should return the notes.html file.
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,19 @@
+const { urlencoded } = require('express');
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// initialize app
+cosnt app = express();
+const PORT = process.env.PORT || 3000;
+
+//body parsing, static, route middleware
+app.use(express.json());
+app.use(express.urlencoded({ extemded: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+//port start
+app.listen(PORT, () => console.log(`Listening to Port: ${PORT}`));
